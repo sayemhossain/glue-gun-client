77263dd948a7df36f2841ddba713c38f38710ae5
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-05-26 20:48:53 +0600

    portfolio section done

--- src/pages/My Portfolio/MyProtfolio.js
+++ src/pages/My Portfolio/MyProtfolio.js
@@ -3,7 +3,7 @@ import portfolio from "../../assets/portfolio.jpg";
 
 const MyProtfolio = () => {
   return (
-    <div className="px-5 md:px-28 min-h-screen mt-10 md:mt-38">
+    <div className="px-5 md:px-28 mb-10 min-h-screen mt-10 md:mt-38">
       <div className="flex justify-between">
         <div>
           <h3 className="text-2xl uppercase font-bold">Shayem Hossain</h3>
@@ -30,7 +30,7 @@ const MyProtfolio = () => {
           <h4 className="font-bold mt-3 text-gray-600 text-2xl mb-2">
             Objective
           </h4>
-          <p>
+          <p className="pl-2">
             I'm Shayem hossain.I am an enthusiastic, self-motivated, reliable,
             responsible, and hard-working person. I'm a Frontend web developer
             who is passionate about making error-free website. I have a passion
@@ -44,16 +44,16 @@ const MyProtfolio = () => {
           <h4 className="font-bold mt-3 text-gray-600 mb-2 text-2xl">
             Skill Highlights
           </h4>
-          <p>
+          <p className="pl-2">
             <span className="font-bold text-gray-600">Expertise:</span> React,
             JavaScript, ES6, REST API, Router, React Hook, React Bootstrap,
             Bootstrap-5, CSS3, HTML5.
           </p>
-          <p>
+          <p className="pl-2">
             <span className="font-bold text-gray-600">Comfortable:</span>{" "}
             MongoDB, Express, Node, Firebase, Tailwind CSS, daisyUI.
           </p>
-          <p>
+          <p className="pl-2">
             <span className="font-bold text-gray-600">Tools:</span> Github, VS
             Code, Chrome Dev Tools, heroku, Netlify, Figma.
           </p>
@@ -62,7 +62,7 @@ const MyProtfolio = () => {
           <h4 className="font-bold text-gray-600 mt-3 mb-2 text-2xl">
             Projects
           </h4>
-          <div>
+          <div className="pl-2">
             <p className="text-xl">
               1. Home Furniture |{" "}
               <a
@@ -92,6 +92,88 @@ const MyProtfolio = () => {
               </span>
             </p>
           </div>
+          <div className="mt-2 pl-2">
+            <p className="text-xl">
+              1. Resto |{" "}
+              <a
+                className="text-blue-500"
+                href="https://sayemhossain.github.io/resto/"
+              >
+                Live website
+              </a>
+            </p>
+            <p className="md:pl-5 mt-1">
+              <span>-This project has been made for a resturant.</span>
+              <br />
+              <span>-This is a Bootstarap based website.</span>
+            </p>
+          </div>
+          <div className="mt-2 pl-2">
+            <p className="text-xl">
+              1. Doctor Portal |{" "}
+              <a
+                className="text-blue-500"
+                href="https://doctor-portal-5c43b.web.app/"
+              >
+                Live website
+              </a>
+            </p>
+            <p className="md:pl-5 mt-1">
+              <span>
+                -This is a doctor portal website here you can book you
+                appointment on our selected time.
+              </span>
+              <br />
+              <span>
+                -This project data load from database mongodb and server heroku
+                and react firebase.
+              </span>
+              <br />
+              <span>
+                -This projects also use react custom link router Node, express.
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h4 className="font-bold mt-3 text-gray-600 text-2xl mb-1">
+          Education
+        </h4>
+        <div className="pl-2">
+          <div>
+            <h5 className="text-lg font-bold">
+              <small>Secondary School Certificate- SSC</small>
+            </h5>
+            <p>Mubbelai Lutfia High School</p>
+            <p>Grade: GPA 5.00</p>
+          </div>
+          <div>
+            <h5 className="text-lg font-bold">
+              <small>Higher Secondary Certificate- HSC</small>
+            </h5>
+            <p>Ullapara Science Collage</p>
+            <p>Grade: GPA 5.00</p>
+          </div>
+          <div>
+            <h5 className="text-lg font-bold">
+              <small>B.S. Computer Science and Engineering- CSE</small>
+            </h5>
+            <p>Daffodil International University</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h4 className="font-bold mt-3 text-gray-600 text-2xl mb-1">
+          Languages
+        </h4>
+        <div className="pl-2">
+          <h5>
+            Bangla- <small>Native</small>
+          </h5>
+          <h5>
+            English- <small>Fluent</small>
+          </h5>
         </div>
       </div>
     </div>
