b4eff52a9b504f70a6e3fdb3c5a0700abba0341c
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-09-13 19:00:40 +0600

    new code added

--- src/pages/Dashboard/AyyKoriOrders/AyyKoriOrders.js
+++ src/pages/Dashboard/AyyKoriOrders/AyyKoriOrders.js
@@ -19,7 +19,7 @@ const AyyKoriOrders = () => {
           className="text-xl md:px-20 mt-5 text-center "
           style={{ fontFamily: "Teko" }}
         >
-          Orders From AyyKori
+          Orders From AyyKor
         </h3>
       </div>
       <hr />
