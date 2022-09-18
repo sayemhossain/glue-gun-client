9c06c84f3dfb4e649bb0c361429f82d97a936f80
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-09-18 18:37:52 +0600

    new code added

--- src/hooks/useToolDetails.js
+++ src/hooks/useToolDetails.js
@@ -6,7 +6,7 @@ const useToolDetails = ({ toolId }) => {
     fetch(`https://intense-cove-25675.herokuapp.com/tools/${toolId}`)
       .then((res) => res.json())
       .then((data) => setTool(data));
-  }, []);
+  }, [tool]);
   return [tool];
 };
 export default useToolDetails;
