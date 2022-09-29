82409f3658fcf067247bd2440e983d43e05dae21
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-09-29 10:20:51 +0600

    new code added

--- src/pages/Home/Purchase.js
+++ src/pages/Home/Purchase.js
@@ -213,7 +213,7 @@ const Purchase = () => {
               ))}
             </p>
             <p>
-              Per piece price:{" "}
+              Pe piece price:{" "}
               <span className="text-gray-600 font-bold border-4 border-gray-50 rounded-2xl">
                 ${price}
               </span>
