b6d93a78f41c3784f7cd0f521a9beea60e388045
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-09-29 12:46:47 +0600

    new cdoe

--- src/pages/Home/Purchase.js
+++ src/pages/Home/Purchase.js
@@ -226,7 +226,7 @@ const Purchase = () => {
                 </span>
               </p>
               <p>
-                Minimu Order Quantity :{" "}
+                Minimum Order Quantity :{" "}
                 <span className="text-red-500 font-bold">
                   {minimum_order_quantity}
                 </span>
