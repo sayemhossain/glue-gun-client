ae75fe14901485ea5d111c2f48a96cbe5a092d3f
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-12-17 00:17:48 +0600

    new

--- src/pages/ContactUs/ContactUs.js
+++ src/pages/ContactUs/ContactUs.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Contact from "../Home/Contact";
+import MessengerCustomerChat from "react-messenger-customer-chat";
 
 const ContactUs = () => {
   return (
     <div>
       <Contact></Contact>
+      <MessengerCustomerChat pageId="323658974633921" appId="512974024134476" />
     </div>
   );
 };
